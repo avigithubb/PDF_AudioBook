f3df25d323307bcc71966d3f10ccd0ba99d40452
Abhishek <avishek.npt@gmail.com>
2024-07-02 21:39:31 +0530

    added region

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ app.secret_key = os.getenv('MY_SECRET_KEY')
 aws_access_key_id = os.getenv('AWS_ACCESS_KEY')
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 session = Session(aws_access_key_id=aws_access_key_id,
-                  aws_secret_access_key=aws_secret_access_key,)
+                  aws_secret_access_key=aws_secret_access_key,
+                  region_name="us-west-2",)
 polly = session.client("polly")
 
 
